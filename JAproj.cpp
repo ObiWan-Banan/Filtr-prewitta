f92429d6b4881bbdef138827636f01c1891bb4c6
Rafał Świderski <raafalsw@gmail.com>
2020-10-15 18:38:58 +0200

    sdfhsdfasdfh

--- JAproj.cpp
+++ JAproj.cpp
@@ -1,7 +1,29 @@
 #include "JAproj.h"
+#include <qfiledialog.h>
+#include <qmessagebox.h>
+#include <qprogressbar.h>
+
 
 JAproj::JAproj(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    
+    
+}
+
+void JAproj::on_quitButton_clicked()
+{
+    close();
+}
+
+void JAproj::on_openButton_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this,tr("Image chooser")," " ,tr("Image Files (*.png *.jpg *.bmp)"));
+    QMessageBox::information(this, tr("file name"), "Selected file path: "+filename);
+}
+
+void JAproj::on_startAlgorithmButton_clicked()
+{
+    close();
 }
--- JAproj.h
+++ JAproj.h
@@ -9,7 +9,11 @@ class JAproj : public QMainWindow
 
 public:
     JAproj(QWidget *parent = Q_NULLPTR);
-
+    
+private slots:
+    void on_quitButton_clicked();
+    void on_openButton_clicked();
+    void on_startAlgorithmButton_clicked();
 private:
     Ui::JAprojClass ui;
 };
